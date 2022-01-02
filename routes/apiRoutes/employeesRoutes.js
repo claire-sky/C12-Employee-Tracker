419dc1a20b0dd40f0481da8014309b19a620b68e
Claire Sky <eclconnolly@gmail.com>
2022-01-01 20:05:51 -0800

    employee routes to add and delete

--- routes/apiRoutes/employeesRoutes.js
+++ routes/apiRoutes/employeesRoutes.js
@@ -18,6 +18,44 @@ router.get('/employees', (req, res) => {
     });
 });
 
+// add an employee
+router.post('/employee/', ({ body }, res) => {
+    const sql = `INSERT INTO employees (id, first_name, last_name, role_id, manager_id)
+        VALUES (?,?,?,?,?)`;
+    const params = [body.id, body.first_name, body.last_name, body.role_id, body.manager_id];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        });
+    });
+});
+
+// delete an employee
+router.delete('/employee/:id', (req, res) => {
+    const sql = `DELETE FROM employees WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: res.message });
+        } else if (!result.affectedRows) {
+            res.json({ message: 'Employee not found'});
+        } else {
+            res.json({
+                message: 'deleted',
+                changes: result.affectedRows,
+                name: req.params.id
+            });
+        }
+    });
+});
+
 // `SELECT id, CONCAT(first_name, ' ', last_name) AS employee_name FROM employees`;
 
 // `SELECT b.id,
